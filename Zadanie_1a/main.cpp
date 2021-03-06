5dcda8f5b67c47ffd6b4088bb79db05dcfb955bb
desmond3625 <klaudiaj24@gmail.com>
2021-03-06 19:26:30 +0100

    initial version

--- Zadanie_1a/main.cpp
+++ Zadanie_1a/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int n = 10; //constant number of input names
+
+    string allNames[n]; //array of strings
+
+    for (int t = 0; t < n; t++) {
+        string str = "";
+        getline(cin, str);   //reads input line
+        allNames[t] = str;  //saves name in the names array
+    }
+
+    //names display loop
+    for (int t = 0; t < n; t++) {
+        cout << allNames[t]<<endl;
+    }
+
     return 0;
 }
