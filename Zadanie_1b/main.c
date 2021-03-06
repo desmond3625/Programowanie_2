c731d1b6f8db21cf8415e5ce8d4f8038ab1d79b4
desmond3625 <klaudiaj24@gmail.com>
2021-03-06 19:15:07 +0100

    initial version

--- Zadanie_1b/main.c
+++ Zadanie_1b/main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+        int n=10; //constant number of input names
+
+        char* allNames[n]; //array of pointers to names
+        char *str;
+
+        char input_letter;
+
+        for(int t=0;t<n;t++) {
+
+            int i = 0;
+            str= (char *) malloc(1 * sizeof(char));
+
+            while (input_letter = getc(stdin), input_letter != '\n') { //reads input line
+                str[i] = input_letter;
+                i++;
+                realloc(str, (i+1) * sizeof(char));   //reallocates memory while adding characters
+            }
+            str[i] = '\0';
+            allNames[t]=str;  //saves pointer to single name in an array
+        }
+
+        //names display loop
+        for(int t=0;t<n;t++){
+            int i=0;
+            while(*(allNames[t]+i)!='\0'){
+                printf("%c", *(allNames[t]+i));
+                i++;
+            }
+            printf("\n");
+        }
+
+        //memory deallocation
+        for(int t=0;t<n;t++){
+            free(allNames[t]);
+        }
+
+        return 0;
 }
